39b98ec2e5a5cd90c4017855a13731daaa766d85
cendanaf <f.cendana@gmail.com>
2022-04-20 10:55:19 -0400

    moved last code as sweep and used a zeroing pwm to the end of the cycle

--- esp32PWM.ino
+++ esp32PWM.ino
@@ -1,45 +1,51 @@
 const int pwm[3] = {4,3,2}; //GPIO{4,3,2}
 const int en[3] = {8,7,6};
+const int pwmChannel[3] = {0,1,2}; //PWM channels (0-15)
 
 int dutyCycle;
 /*Frequency: max 40MHz*/
 const int PWMFreq = 5000; //5 KHz
 /*Resolution: 1-16 bits*/
-const int PWMResolution = 10;
+const int PWMResolution = 10; //1-16 bits (10 bits = 0-1023)
 /*Duty cycle dependent on resolution*/
 const int MAX_DUTY_CYCLE = (int)(pow(2, PWMResolution) - 1);
-/*PWM channels: 0-15*/
-const int pwmChannel[3] = {0,1,2};
+
+
+
 void setup()
 {
   /*Setup enable pins*/
-  for(int i=0; i<3; i++){
+  for(int i=0; i<3; i++)
+  {
     pinMode(en[i], OUTPUT);
   }
   
   /*Initialize channels*/
-  for(int i=0; i<3; i++){
+  for(int i=0; i<3; i++)
+  {
     ledcSetup(pwmChannel[i], PWMFreq, PWMResolution);
   }
-  /* Attach the PWM Channels to the GPIO Pin */
-  for(int i=0; i<3; i++){
+  /* Route PWM channel signals to GPIO pins */
+  for(int i=0; i<3; i++)
+  {
     ledcAttachPin(pwm[i], pwmChannel[i]);
   }
 }
+
 void loop()
 {
-  for(int i=0; i<3; i++){
-    digitalWrite(en[i], HIGH);
-    for(dutyCycle = 0; dutyCycle <= MAX_DUTY_CYCLE; dutyCycle++)
-    {
-      ledcWrite(pwmChannel[i], dutyCycle);
-      delay(1);
-    }
-    for(dutyCycle = MAX_DUTY_CYCLE; dutyCycle >= 0; dutyCycle--)
-    {
-      ledcWrite(pwmChannel[i], dutyCycle);
-      delay(1);
-    }
-    digitalWrite(en[i], LOW);
+  for(int i=0; i<3; i++)
+  {
+    runPhase(en[i], pwmChannel[i], 50);
+    delay(1);
+    runPhase(en[i], pwmChannel[i], 0);
   }
+}
+
+
+void runPhase(int en, int pwmChannel, int dutyCycle){
+  digitalWrite(en, HIGH);
+  ledcWrite(pwmChannel, dutyCycle);
+  delay(20);
+  digitalWrite(en, LOW);
 }
