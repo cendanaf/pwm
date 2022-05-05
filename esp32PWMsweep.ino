d2c97a2d3a5318dca8d76e5e0bfa8751b3946860
arjun <f.cendana@gmail.com>
2022-05-05 07:33:54 -0400

    sine function for ledcWrite

--- esp32PWMsweep.ino
+++ esp32PWMsweep.ino
@@ -1,20 +1,35 @@
 const int pwm[3] = {4,3,2}; //GPIO{4,3,2}
 const int en[3] = {8,7,6};
+const int pwmChannel[3] = {0,1,2}; //PWM channels (0-15)
 
 int dutyCycle;
 /*Frequency: max 40MHz*/
-const int PWMFreq = 5000; //5 KHz
+const int PWMFreq = 10000; //Hz
 /*Resolution: 1-16 bits*/
 const int PWMResolution = 10;
 /*Duty cycle dependent on resolution*/
 const int MAX_DUTY_CYCLE = (int)(pow(2, PWMResolution) - 1);
-/*PWM channels: 0-15*/
-const int pwmChannel[3] = {0,1,2};
+
+const int LIMIT_MAX_DUTY_CYCLE = (int) (1 * MAX_DUTY_CYCLE);
+
+int pwmA[MAX_DUTY_CYCLE];
+int pwmB[MAX_DUTY_CYCLE];
+int pwmC[MAX_DUTY_CYCLE];
+
+float phaseB = 3.14 * (30./180.);
+float phaseC = 3.14 * (60./180.);
+
+const int d = 5;
+boolean forward = true;
+int phase = 0;
+int increment;
+
 void setup()
 {
   /*Setup enable pins*/
   for(int i=0; i<3; i++){
     pinMode(en[i], OUTPUT);
+    digitalWrite(en[i], HIGH);
   }
   
   /*Initialize channels*/
@@ -25,21 +40,41 @@ void setup()
   for(int i=0; i<3; i++){
     ledcAttachPin(pwm[i], pwmChannel[i]);
   }
+
+  /*Generate table of values for sine function at different phases*/
+  for(int i=0; i<MAX_DUTY_CYCLE; i++){
+    float angleA = (2 * 3.14 * i) / MAX_DUTY_CYCLE;
+    float angleB = angleA + phaseB;
+    float angleC = angleA + phaseC;
+
+    float sinA = 0.5 * (sin(angleA) + 1);
+    float sinB = 0.5 * (sin(angleB) + 1);
+    float sinC = 0.5 * (sin(angleC) + 1);
+
+    pwmA[i] = (int) (LIMIT_MAX_DUTY_CYCLE * sinA);
+    pwmB[i] = (int) (LIMIT_MAX_DUTY_CYCLE * sinB);
+    pwmC[i] = (int) (LIMIT_MAX_DUTY_CYCLE * sinC);
+    
+  }
+
+  Serial.begin(9600);
 }
+
 void loop()
 {
-  for(int i=0; i<3; i++){
-    digitalWrite(en[i], HIGH);
-    for(dutyCycle = 0; dutyCycle <= MAX_DUTY_CYCLE; dutyCycle++)
-    {
-      ledcWrite(pwmChannel[i], dutyCycle);
-      delay(1);
-    }
-    for(dutyCycle = MAX_DUTY_CYCLE; dutyCycle >= 0; dutyCycle--)
-    {
-      ledcWrite(pwmChannel[i], dutyCycle);
-      delay(1);
-    }
-    digitalWrite(en[i], LOW);
-  }
+  ledcWrite(pwmChannel[0], pwmA[phase]);
+  ledcWrite(pwmChannel[1], pwmB[phase]);
+  ledcWrite(pwmChannel[2], pwmC[phase]);
+
+  if(forward) increment = 1;
+  else increment = -1;
+
+  phase = phase + increment;
+  if(phase > MAX_DUTY_CYCLE) phase = 0;
+  if(phase < 0) phase = MAX_DUTY_CYCLE;
+
+  Serial.println(String(pwmA[phase]) + ", " + String(pwmB[phase]) + ", " + String(pwmC[phase]));
+
+  delay(d);
+  
 }
