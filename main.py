5da6af3cdafd43ba057b13ac2a292e1d4843bcb3
arjun <f.cendana+github@gmail.com>
2024-08-08 17:55:28 -0400

    Attempt to send 3 sinusoidal pwm signals 120 degrees apart on pins 1,2,3 and into either pin 4, 5, or 6

---
+++ main.py
@@ -0,0 +1,81 @@
+from machine import Pin, PWM
+import math
+import time
+import sys
+
+hsa = PWM(Pin(1))
+hsb = PWM(Pin(2))
+hsc = PWM(Pin(3))
+
+f = 20000
+hsa.freq(f)
+hsb.freq(f)
+hsc.freq(f)
+
+voltageResolution = 16 # bits
+nVoltages = (2**voltageResolution) - 1
+dutyCycleLimiter = 0.5
+duty = int(nVoltages/2)
+
+lsa = Pin(4, Pin.OUT)
+lsb = Pin(5, Pin.OUT)
+lsc = Pin(6, Pin.OUT)
+
+button = Pin(9, Pin.IN, Pin.PULL_DOWN)
+
+sinusoidPeriod = 500 # milliseconds
+angularFrequency = 2 * math.pi / (sinusoidPeriod)
+
+tB = 90 / angularFrequency
+tC = 210 / angularFrequency
+tA = 330 / angularFrequency
+
+def Sinusoid(t, phase, limit, omega=angularFrequency, p2p=maxDutyCycle):
+    amplitude = p2p / 2
+    angle = omega * t + phase
+    value = math.sin(angle)
+    duty = amplitude * (value + 1)
+    return int(limit * duty)
+
+def SetLowSide(dt, A=lsa, B=lsb, C=lsc, tA=tA, tB=tB, tC=tC):
+    #print(dt)
+    if A.value():
+        #print("LSA high")
+        if dt >= tA:
+            A.low()
+            B.high()
+        return
+    elif B.value():
+        #print("LSB high")
+        if dt >= tB:
+            B.low()
+            C.high()
+            if dt > tC:
+                C.low()
+        return
+    elif C.value():
+        #print("LSC high")
+        if dt >= tC:
+            C.low()
+            A.high()
+            if dt > tA:
+                A.low()
+        return
+    else:
+        B.high()
+        return
+    
+t0 = time.ticks_ms()
+while True:
+    dt = time.ticks_ms() - t0
+    dt %= sinusoidPeriod
+            
+    SetLowSide(dt) 
+    
+    da = Sinusoid(dt, 0, dutyCycleLimiter)
+    db = Sinusoid(dt, 120, dutyCycleLimiter)
+    dc = Sinusoid(dt, 240, dutyCycleLimiter)
+            
+    hsa.duty_u16(da)
+    hsb.duty_u16(db)
+    hsc.duty_u16(dc)
