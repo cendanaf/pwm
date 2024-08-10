1f99d6385b86dcaf8670dc9262dabea65d0e28e5
arjun <f.cendana+github@gmail.com>
2024-08-10 14:57:00 -0400

    Minor changes

--- pico.py
+++ pico.py
@@ -3,82 +3,84 @@ import math
 import time
 import sys
 
-hsa = PWM(Pin(0))
-lsa = Pin(1, Pin.OUT)
+HS = [ PWM(Pin(i)) for i in [0,2,4] ]
+LS = [ Pin(i, Pin.OUT) for i in [1,3,5] ]
 
-hsb = PWM(Pin(2))
-lsb = Pin(3, Pin.OUT)
+f = 80000
+for i in HS:
+    i.freq(f)
 
-hsc = PWM(Pin(4))
-lsc = Pin(5, Pin.OUT)
-
-hsd = PWM(Pin(6)) # reference (for oscilloscope)
-
-f = 20000
-hsa.freq(f)
-hsb.freq(f)
-hsc.freq(f)
+hsRef = PWM(Pin(6)) # Scope reference
+hsRef.freq(f)
 
 voltageResolution = 16 # bits
 nVoltages = (2**voltageResolution) - 1
-dutyCycleLimiter = 0.5
-duty = int(nVoltages/2)
-
-button = Pin(9, Pin.IN, Pin.PULL_DOWN)
+dutyCycleLimiter = 0.3
 
 sinusoidPeriod = 500 # milliseconds
 angularFrequency = 2 * math.pi / (sinusoidPeriod)
 
-tB = 90 / angularFrequency
-tC = 210 / angularFrequency
-tA = 330 / angularFrequency
-
-def Sinusoid(t, phase, limit, omega=angularFrequency, p2p=maxDutyCycle):
+def Sinusoid(t, phase, limit=dutyCycleLimiter, omega=angularFrequency, p2p=nVoltages):
     amplitude = p2p / 2
-    angle = omega * t + phase
+    angle = omega * t + (phase * (2 * math.pi / 360))
     value = math.sin(angle)
-    duty = amplitude * (value + 1)
+    duty = amplitude * value
     return int(limit * duty)
 
-def SetLowSide(dt, A=lsa, B=lsb, C=lsc, tA=tA, tB=tB, tC=tC):
-    #print(dt)
-    if A.value():
-        #print("LSA high")
-        if dt >= tA:
-            A.low()
-            B.high()
-        return
-    elif B.value():
-        #print("LSB high")
-        if dt >= tB:
-            B.low()
-            C.high()
-            if dt > tC:
-                C.low()
-        return
-    elif C.value():
-        #print("LSC high")
-        if dt >= tC:
-            C.low()
-            A.high()
-            if dt > tA:
-                A.low()
+def SetHighSide(phase, pwm):
+    phase.duty_u16(pwm)
+    return
+
+def SetLowSide(phase, value):
+    if value:
+        phase.high()
         return
     else:
-        B.high()
+        phase.low()
         return
+
+def Step(highSide, lowSide):
+    if highSide == 0:
+        if lowSide == 1:
+            return 1
+        else:
+            return 2
+    elif highSide == 1:
+        if lowSide == 0:
+            return 5
+        else:
+            return 6
+    else:
+        if lowSide == 0:
+            return 4
+        else:
+            return 3
     
 t0 = time.ticks_ms()
+hsRef.duty_u16(int(nVoltages/2)) 
+
+phaseH = 0
+phaseL = 0
+step = 0
 while True:
-    dt = time.ticks_ms() - t0
+    dt = time.ticks_diff(time.ticks_ms(), t0)
     dt %= sinusoidPeriod
-            
-    SetLowSide(dt) 
+    DUTY = [Sinusoid(dt, 0), Sinusoid(dt, 120), Sinusoid(dt, 240)]
+    maxI = DUTY.index(max(DUTY))
+    minI = DUTY.index(min(DUTY))
     
-    da = Sinusoid(dt, 0, dutyCycleLimiter)
-    db = Sinusoid(dt, 120, dutyCycleLimiter)
-    dc = Sinusoid(dt, 240, dutyCycleLimiter)
-            
-    hsa.duty_u16(da)
-    hsb.duty_u16(db)
-    hsc.duty_u16(dc)
+    if maxI != phaseH:
+        SetHighSide(HS[phaseH], 0)
+        phaseH = maxI
+        #step = Step(phaseH, phaseL)
+        #print(f"{dt}: {phaseH} -> {phaseL}, step {step}")
+    if minI != phaseL:
+        SetLowSide(LS[phaseL], 0)
+        phaseL = minI
+        SetLowSide(LS[phaseL], 1)
+        #step = Step(phaseH, phaseL)
+        #print(f"{dt}: {phaseH} -> {phaseL}, step {step}")
+    
+    
+    SetHighSide(HS[phaseH], DUTY[phaseH])
+
