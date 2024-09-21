92dbf1ef7805f33b9600cd5a08f400180e09bf10
arjun <f.cendana+github@gmail.com>
2024-09-21 12:46:04 -0400

    Solution was to set at half the pwm but run it twice

--- piopwm/main.py
+++ piopwm/main.py
@@ -3,29 +3,30 @@ import machine
 import utime
 import rp2
 
-time_resolution = 16 # bits
+time_resolution = 10 # bits
 time_slices = (2 ** time_resolution) - 1
-# Create PIOPWM object used to fade LED
-# (state machine 0, pin 25, max PWM count of 65535, PWM freq of 10 MHz)
-pwm = PIOPWM(0, 17, 26, max_count=time_slices, count_freq=10_000_000)
+
+pwm = PIOPWM(0, 17, 26, max_count=time_slices, count_freq=25_650_000)
 
 #duty = int(0.2 * time_slices)
 #pwm.Set(duty)
 
-
-i = 0
+i = int(0.2 * time_slices)
 pwm.Set(i)
+
+#i = 0
 try:
     while True:
         adc = pwm.GetADCvalue()
         print(f"{i}, {adc}")
-        i += 10
-        i %= int(0.9 * (time_slices + 1))
-        pwm.Set(i)
+        #i += 10
+        #i %= int(0.9 * (time_slices + 1))
+        #pwm.Set(i)
         utime.sleep_ms(1000)
 except KeyboardInterrupt:
     print("Ending program")
     pwm.End()
 finally:
     print("Program end")
-    
+
+
--- piopwm/piopwm.py
+++ piopwm/piopwm.py
@@ -5,65 +5,57 @@ import time
 
 @asm_pio(sideset_init=(PIO.OUT_LOW, PIO.OUT_LOW))
 def PWM():
-    pull(noblock)    .side(0b00)
+    pull(noblock)  .side(0b00)
     mov(x, osr) 
     mov(y, isr) 
     
-    label("pwmloop1") # 1st stage turns side pin on
+    label("pwmloop1")
     jmp(x_not_y, "skip1")
-    nop()         .side(0b11) 
-    jmp("transition")
+    nop()          .side(0b01)
     
     label("skip1")
     jmp(y_dec, "pwmloop1")
     
-    label("transition") # Transition stage (supposedly) doubles the value of x
-    mov(x, x << 1)
-    jmp("pwmloop2")
+    irq(0)         .side(0b11) 
+    mov(y, x)     
     
-    label("pwmloop2") # 2nd stage raises flag (at midpoint)
-    jmp(x_not_y, "skip2")
-    nop()            .side(0b01)
+    label("stage2")
+    nop() [0]        # Use this for adjusting sampling point
+    jmp(y_dec, "stage2")    
     
-    label("skip2")
-    jmp(y_dec, "pwmloop2")
-
 
 class PIOPWM:
     def __init__(self, sm_id, pwm_pin, adc_pin, max_count, count_freq):
         self._marker = Pin(0, Pin.OUT)
         self._adc = ADC(adc_pin)
         self._adc_value = 0
-        self._half_pulse = 0
         self._count_freq = count_freq
         self._max_count = max_count
-        self._sm = StateMachine(sm_id, PWM, freq=2 * count_freq, sideset_base=Pin(pwm_pin))
-        #self._sm.irq(lambda pio: print(pio.irq().flags()))
-        self._sm.irq(handler=self.Sample)
+        self._PWMsm = StateMachine(sm_id,
+                                   PWM,
+                                   freq=2 * count_freq,
+                                   sideset_base=Pin(pwm_pin))
+        self._PWMsm.irq(handler=self.Sample)
         # Use exec() to load max count into ISR
-        self._sm.put(max_count)
-        self._sm.exec("pull()")
-        self._sm.exec("mov(isr, osr)")
-        self._sm.active(1)
+        self._PWMsm.put(max_count)
+        self._PWMsm.exec("pull()")
+        self._PWMsm.exec("mov(isr, osr)")
+        self._PWMsm.active(1)
         
     def Set(self, value):
         # Minimum value is -1 (completely turn off), 0 actually still produces narrow pulse
         value = max(value, -1)
         value = min(value, self._max_count)
-        self._half_pulse = int((value * 1_000_000) / (2 * self._count_freq) )
-        self._sm.put(value)
+        value = int((value-62) / 2)
+        self._PWMsm.put(value)
         
     def Sample(self, sm):
-        #self._marker.value(1)
-        #t0 = time.time_ns()
-        #utime.sleep_us(self._half_pulse)
+        #print("Sampling")
         self._adc_value = self._adc.read_u16()
-        #t = time.time_ns()
-        #print(f"{t-t0}")
-        #self._marker.value(0)
-    
+        
     def GetADCvalue(self):
         return self._adc_value
         
     def End(self):
-        self._sm.active(0)
+        self._PWMsm.active(0)
+        
